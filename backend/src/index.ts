c7be6290dd12ad14c4198eabb81b9ffef4593e8c
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-11 19:26:04 +0200

    mockDB in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,19 @@
 import http from "http";
+
+const mockDB = [
+  {
+    id: 1,
+    date: "2024-10-10",
+    duration: "00:50:00",
+    distance: 10,
+  },
+  {
+    id: 1,
+    date: "2024-10-10",
+    duration: "00:50:00",
+    distance: 10,
+  },
+];
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
