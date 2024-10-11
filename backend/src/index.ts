800221db944295736ce40448fb153c6390720d53
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-11 20:11:26 +0200

    return mockDB from BE server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import http from "http";
+import { mock } from "node:test";
 
 const mockDB = [
   {
@@ -8,7 +9,7 @@ const mockDB = [
     distance: 10,
   },
   {
-    id: 1,
+    id: 2,
     date: "2024-10-10",
     duration: "00:50:00",
     distance: 10,
@@ -19,7 +20,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     "content-Type": "application/json",
   });
-  const result = JSON.stringify({ ok: true });
+  const result = JSON.stringify({ mockDB });
   res.write(result);
   res.end();
 });
