cd2fba3e809d8cf968624166fc057257eb63f105
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-11 18:58:39 +0200

    print port for backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import http from "http";
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, {
-      "content-Type": "application/json",
-    });
-    const result = JSON.stringify({ ok: true });
-    res.write(result);
-    res.end();
-  })
-  .listen(8080);
+const port = 8080;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "content-Type": "application/json",
+  });
+  const result = JSON.stringify({ ok: true });
+  res.write(result);
+  res.end();
+});
+server.listen(port, () => {
+  console.log(`server running on http://localhost:${port}/`);
+});
