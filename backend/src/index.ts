0987d4599faf092fca16b18338f578f4b0882c67
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-11 18:46:05 +0200

    add scripts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("ts test1");
+import http from "http";
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "content-Type": "application/json",
+    });
+    const result = JSON.stringify({ ok: true });
+    res.write(result);
+    res.end();
+  })
+  .listen(8080);
