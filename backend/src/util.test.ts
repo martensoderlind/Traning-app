0a477cf32913c897b0e7b1b6e5b1e6d8c7640cb0
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-13 19:32:48 +0200

    validation test

--- backend/src/util.test.ts
+++ backend/src/util.test.ts
@@ -1,9 +1,9 @@
 import { deepEqual } from "node:assert";
-import { dateControll, distanceControll } from "./util";
+import { dateControll, distanceControll, validation } from "./util";
 import test from "node:test";
 
 test("should return false of submitDate>todaysDate", () => {
-  const todaysDate = "2024-10-13";
+  const todaysDate = new Date();
   const submitDate = "2024-10-14";
   const result = dateControll(todaysDate, submitDate);
   deepEqual(result, false);
@@ -12,4 +12,14 @@ test("should return false of submitDate>todaysDate", () => {
 test("should return false if submitDistance < 0", () => {
   const result = distanceControll(-1);
   deepEqual(result, false);
+});
+test("should return true if both distanceControll and dateControll are true", () => {
+  const todaysDate = new Date();
+  const submitData = {
+    date: "2024-10-12",
+    distance: 1,
+    duration: "50:00",
+  };
+  const result = validation(submitData, todaysDate);
+  deepEqual(result, true);
 });
