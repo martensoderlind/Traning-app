d2ce0e37a8e44012f53bb7a3a51fe5fab59c47c6
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-13 12:05:49 +0200

    removed console.logs and addSession call

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,7 +4,6 @@ const db = new Database("./database.db");
 db.prepare(
   "CREATE TABLE IF NOT EXISTS sessions (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, distance NUMBER, duration TEXT)"
 ).run();
-console.log("db created");
 
 export function addSession(date: string, distance: number, duration: string) {
   const newInput = db.prepare(
@@ -13,5 +12,3 @@ export function addSession(date: string, distance: number, duration: string) {
   newInput.run(date, distance, duration);
   console.log("session added!");
 }
-
-addSession("2024-10-13", 11, "59:44");
