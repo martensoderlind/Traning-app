690561431e3ef44710687cc534219b3fd8fd2d3b
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-10-13 19:32:38 +0200

    validation function

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -12,11 +12,21 @@ export function addValuesToDB(newInput: DataInput) {
   db.push(newDbValue);
 }
 
-export function dateControll(todaysDate: string, submitDate: string) {
-  const inputDate = new Date(submitDate);
+export function dateControll(todaysDate: Date, submitDate: string) {
+  const [year, month, day] = submitDate.split("-").map(Number);
+  const inputDate = new Date(year, month - 1, day);
   const today = new Date(todaysDate);
   return inputDate.getTime() <= today.getTime();
 }
 export function distanceControll(submitDistance: number) {
   return submitDistance > 0;
+}
+
+export function validation(session: any, todaysDate: Date) {
+  const date = dateControll(todaysDate, session.date);
+  const distance = distanceControll(session.distance);
+  if (date && distance) {
+    return true;
+  }
+  return false;
 }
